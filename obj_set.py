a626ad1a6e3f5de5d9a4e193433bdb41edfccbb2
WalterCoti <3064960620401@ingenieria.usac.edu.gt>
2020-09-16 23:51:29 -0600

    Clase para set's de memoria

---
+++ obj_set.py
@@ -0,0 +1,14 @@
+class Objeto_Set():
+    def __init__(self, name_Set):     
+        self.nameSet = name_Set
+        self.list_SetElement = []
+
+    def setElementList(self, objetoesto):
+        self.list_SetElement.append(objetoesto)
+
+    def getlist(self):
+        return self.list_ESet
+
+    def getnombre(self):
+        return self.nameSet
+
