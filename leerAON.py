b9cfb67b9b75f477ac77eb29450f27cf4a2684b3
WalterCoti <69011743+WalterCoti@users.noreply.github.com>
2020-09-22 15:04:04 -0600

    cambio por 'readFile', 'leeraon' lee contenido aon

--- leerAON.py
+++
@@ -1,8 +0,0 @@
-
-def leerArchivo(self, set_id, path):
-    my_path = os.path.abspath(os.path.dirname(__file__))
-    path = os.path.join(my_path, path)
-    
-    with open (path, 'r') as data: 
-        contenido = data.read()
-        print("Archivos cargados con exito\n")
